72576d7a0f9516c1b2bd67d453f40f9d972eed32
Bernard Igiri <bernard.igiri@gmail.com>
2020-08-07 21:20:08 -0400

    Messing around with rust.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let a:[bool; 5] = [true, false, true, false, false];
+    let s = String::from("Hello everybody!");
+    let hello = &s[0..5];
+    let everybody = &s[6..15];
+    let s2 = &s;
+    println!("{} {} {} {}", hello, everybody, s2, a[2]);
+    for n in (1..10).rev() {
+        println!("{}", n);
+    }
+    println!("Let's go!");
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("Guess a number from 1 to 100.");
+    loop {
+        let mut input = String::new();
+        println!("Enter your guess:");
+        io::stdin().read_line(&mut input).expect("Failed to read line!");
+        let guess:u32 = input.trim().parse().expect("Please type a number.");
+        let (prompt, done) = match guess.cmp(&secret) {
+            Ordering::Less => ("less than", false),
+            Ordering::Greater => ("greater than", false),
+            Ordering::Equal => ("equal to", true),
+        };
+        println!("Your guess {} was {} the secret.", guess, prompt);
+        if done {
+            break;
+        }
+    }
+}
