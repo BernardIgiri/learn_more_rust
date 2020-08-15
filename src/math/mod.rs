7f6ba001dfce7975ba398915cde805217070d387
Bernard Igiri <bernard.igiri@gmail.com>
2020-08-15 18:41:22 -0400

    Renamed iterator.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,16 +1,16 @@
-pub struct FibIterator {
+pub struct FibonacciSequence {
     previous: u32,
     second_previous: u32,
     result: Option<u32>,
 }
 
 pub fn fib(n: u32) -> Option<u32> {
-    let mut fib_it = FibIterator::new();
+    let mut fib_it = FibonacciSequence::new();
     fib_it.nth(n as usize)
 }
 
 pub fn fib_closest_inverse(n: u32) -> u32 {
-    let fib_it = FibIterator::new();
+    let fib_it = FibonacciSequence::new();
     let mut previous = 0;
     let mut count = 0;
     for value in fib_it {
@@ -28,16 +28,16 @@ pub fn fib_closest_inverse(n: u32) -> u32 {
 }
 
 pub fn fib_perfect_inverse(n: u32) -> Option<u32> {
-    let mut fib_it = FibIterator::new();
+    let mut fib_it = FibonacciSequence::new();
     return match fib_it.position(|value| value == n) {
         Some(v) => Some(v as u32),
         None => None,
     };
 }
 
-impl FibIterator {
-    pub fn new() -> FibIterator {
-        FibIterator {
+impl FibonacciSequence {
+    pub fn new() -> FibonacciSequence {
+        FibonacciSequence {
             previous: 1,
             second_previous: 0,
             result: Some(0),
@@ -45,7 +45,7 @@ impl FibIterator {
     }
 }
 
-impl Iterator for FibIterator {
+impl Iterator for FibonacciSequence {
     type Item = u32;
     fn next(&mut self) -> Option<u32> {
         let output = self.result;
