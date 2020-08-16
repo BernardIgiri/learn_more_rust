d36fbe9dfd563215a89253248d2b300c36e2707d
Bernard Igiri <bernard.igiri@gmail.com>
2020-08-16 18:20:07 -0400

    Added readme.

---
+++ src/data_types/README.md
@@ -0,0 +1,3 @@
+# Learn More Rust
+
+Just messing around with Rust Lang to get a deeper understanding of it.
--- src/data_types/mod.rs
+++ src/data_types/mod.rs
@@ -1,17 +1,24 @@
 use std::ops::Deref;
+use std::fmt::Display;
 
-pub struct MyBox<T>(T);
+pub struct MyBox<T: Display>(T);
 
-impl<T> MyBox<T> {
+impl<T: Display> MyBox<T> {
     pub fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
 }
 
-impl<T> Deref for MyBox<T> {
+impl<T: Display> Deref for MyBox<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
         &self.0
+    }
+}
+
+impl<T: Display> Drop for MyBox<T> {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
     }
 }
